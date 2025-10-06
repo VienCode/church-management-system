d9898e3312b41dc01e0ca3876205eaa68f3831c3
VienCode <viensantos24@gmail.com>
2025-10-06 19:22:52 +0800

    edited the database conenction

--- database.php
+++ database.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "", "your_database_name");
+$mysqli = new mysqli("localhost", "root", "", "ucf_database");
 
 if ($mysqli->connect_error) {
     die("Connection failed: " . $mysqli->connect_error);
