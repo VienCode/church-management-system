71eada498053e3a64c83e389d06d64ee1a3b7530
VienCode <viensantos24@gmail.com>
2025-10-06 19:58:58 +0800

    testchange

--- pre-index.php
+++ pre-index.php
@@ -20,7 +20,7 @@
         </div>
 
         <div class="welcome_text">
-            <h1> WELCOME TO THE UNITY CHRISTIAN FELLOWSHIP FAMILY! </h1>
+            <h1> WELCOME TO UNITY CHRISTIAN FELLOWSHIP FAMILY! </h1>
         </div>
 
         <div class="button_container">
