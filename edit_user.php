6d6d9e33ba2cf53723609c13ce7f8ffc2f68e391
VienCode <viensantos24@gmail.com>
2025-10-07 16:47:17 +0800

    temporary fixes on edit user

--- edit_user.php
+++ edit_user.php
@@ -11,6 +11,10 @@ $role_id = $_POST['role_id'];
 
 $stmt = $mysqli->prepare("UPDATE users SET firstname=?, lastname=?, email=?, role_id=? WHERE id=?");
 $stmt->bind_param("sssii", $firstname, $lastname, $email, $role_id, $id);
+if ($role_id < 1 || $role_id > 8) {
+    die("Invalid role_id value ($role_id). Must be between 1 and 8.");
+}
+
 $stmt->execute();
 
 header("Location: admin_dashboard.php");
