0bc9ae9bec03deee4cba8e681bece1c2f728dfed
VienCode <viensantos24@gmail.com>
2025-10-10 20:10:08 +0800

    tweaks on the access

--- cell_group.php
+++ cell_group.php
@@ -3,7 +3,7 @@ include 'database.php';
 include 'auth_check.php';
 
 // Ensure user is logged in and has a role
-if (!isset($_SESSION['user_id']) || !isset($_SESSION['user_role_id'])) {
+if (!isset($_SESSION['user_id']) || !isset($_SESSION['role_id'])) {
     header("Location: login.php");
     exit;
 }
@@ -11,7 +11,7 @@ if (!isset($_SESSION['user_id']) || !isset($_SESSION['user_role_id'])) {
 // Allow only Admin, Leader, or Member roles
 restrict_to_roles([ROLE_ADMIN, ROLE_LEADER, ROLE_MEMBER]);
 
-$user_role = $_SESSION['user_role_id'];
+$user_role = $_SESSION['role_id'];
 $user_id = $_SESSION['user_id'];
 $today = date('Y-m-d');
 
